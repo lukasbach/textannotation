236fe8ab386f3933b57e9367c72d84d41b0a6e82
Lukas Bach <lbach@outlook.de>
2019-11-24 15:05:41 +0100

    feat(control-view): Added dummy-window for editing profiles
    #1

--- bundles/edu.kit.textannotation.annotationplugin/src/edu/kit/textannotation/annotationplugin/AnnotationControlsView.java
+++ bundles/edu.kit.textannotation.annotationplugin/src/edu/kit/textannotation/annotationplugin/AnnotationControlsView.java
@@ -1,5 +1,6 @@
 package edu.kit.textannotation.annotationplugin;
 
+import java.awt.Event;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -7,15 +8,21 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.ui.part.*;
 import org.eclipse.jface.viewers.*;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowData;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.jface.action.*;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.*;
 import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.SWT;
 import org.eclipse.core.runtime.IAdaptable;
@@ -52,6 +59,7 @@ public class AnnotationControlsView extends ViewPart {
 	private Button buttonMakeVerb;
 	private Button buttonMakeObjective;
 	private Button buttonEditProfile;
+	private Button buttonNewProfile;
 
 	@Inject IWorkbench workbench;
 
@@ -60,11 +68,17 @@ public class AnnotationControlsView extends ViewPart {
 		layout = new GridLayout(1, false);
 		parent.setLayout(layout);
 
-		profileSelector = new Combo(parent, SWT.DROP_DOWN | SWT.BORDER);
+		Composite selectorComposite = new Composite(parent, SWT.NONE);
+		selectorComposite.setLayout(new GridLayout(3, false));
+		
+		
+
+		profileSelector = new Combo(selectorComposite, SWT.DROP_DOWN | SWT.BORDER);
 		buttonMakeSubstantive = new Button(parent, SWT.PUSH | SWT.FILL);
 		buttonMakeVerb = new Button(parent, SWT.PUSH | SWT.FILL);
 		buttonMakeObjective = new Button(parent, SWT.PUSH | SWT.FILL);
-		buttonEditProfile = new Button(parent, SWT.PUSH | SWT.FILL);
+		buttonEditProfile = new Button(selectorComposite, SWT.PUSH);
+		buttonNewProfile = new Button(selectorComposite, SWT.PUSH);
 
         profileSelector.add("Requirements Engineering");
         profileSelector.add("Text Analysis");
@@ -74,10 +88,47 @@ public class AnnotationControlsView extends ViewPart {
 		buttonMakeVerb.setText("Make Verb");
 		buttonMakeObjective.setText("Make Objective");
 		buttonEditProfile.setText("Edit Profile");
+		buttonNewProfile.setText("New Profile");
 		
-		for (Control c: Arrays.asList(profileSelector, buttonMakeSubstantive, buttonMakeVerb, buttonMakeObjective, buttonEditProfile)) {
+		buttonEditProfile.addListener(SWT.Selection, new Listener() {
+		    @Override
+			public void handleEvent(org.eclipse.swt.widgets.Event event) {
+		        onEditProfile();
+		    }
+		});
+		
+		for (Control c: Arrays.asList(selectorComposite, profileSelector, buttonMakeSubstantive, buttonMakeVerb, buttonMakeObjective)) {
 			c.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		}
+	}
+	
+	/* TODO refactor to be in separate file */
+	private void onEditProfile() {
+        Display display = PlatformUI.getWorkbench().getDisplay();
+        Shell shell = new Shell(display, SWT.TITLE | SWT.MIN | SWT.CLOSE);
+        shell.setText("Edit Profile");
+        shell.setSize(320, 550);
+
+		RowLayout layout = new RowLayout(SWT.VERTICAL);
+		layout.spacing = 10;
+		layout.marginHeight = 10;
+		layout.marginWidth = 10;
+        shell.setLayout(layout);
+
+        final List list = new List(shell, SWT.BORDER | SWT.V_SCROLL);
+        list.setLayoutData(new RowData(240, 380));
+        
+        list.add("Apple");
+        list.add("Apricot");
+        list.add("Banana");
+        list.add("Carrot");
+        list.add("Cherry");
+        
+        // Button addItem = new Button(shell, SWT.PUSH | SWT.FILL);
+        // addItem.setText("Add Item");
+        // addItem.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+
+        shell.open();
 	}
 
 	@Override
