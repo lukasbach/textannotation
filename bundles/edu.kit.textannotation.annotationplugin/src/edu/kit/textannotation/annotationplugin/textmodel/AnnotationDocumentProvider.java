640198e97bbdd415f2f73f732ae024f1fa913d46
Lukas Bach <lbach@outlook.de>
2019-12-20 00:43:12 +0100

    feat(text-model): use xml as internal data structure

--- bundles/edu.kit.textannotation.annotationplugin/src/edu/kit/textannotation/annotationplugin/textmodel/AnnotationDocumentProvider.java
+++ bundles/edu.kit.textannotation.annotationplugin/src/edu/kit/textannotation/annotationplugin/textmodel/AnnotationDocumentProvider.java
@@ -1,11 +1,13 @@
 package edu.kit.textannotation.annotationplugin.textmodel;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.editors.text.FileDocumentProvider;
@@ -13,36 +15,38 @@ import org.eclipse.ui.texteditor.AbstractDocumentProvider;
 
 import edu.kit.textannotation.annotationplugin.EventManager;
 import edu.kit.textannotation.annotationplugin.profile.AnnotationProfile;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 public class AnnotationDocumentProvider extends FileDocumentProvider {
 	private TextModelIntegration tmi;
 	private AnnotationSet annotationSet;
 	private AnnotationProfile annotationProfile;
 	
-	public class InitializeEvent {
+	public static class InitializeEvent {
 		public AnnotationData annotationData;
 		public IDocument document;
 		
-		public InitializeEvent(AnnotationData annotationData, IDocument document) {
+		InitializeEvent(AnnotationData annotationData, IDocument document) {
 			this.annotationData = annotationData;
 			this.document = document;
 		}
 	}
 	
-	public final EventManager<InitializeEvent> initializeEvent = new EventManager<InitializeEvent>();
+	public final EventManager<InitializeEvent> initializeEvent = new EventManager<>();
 	
 	@Override
 	protected void doSaveDocument(IProgressMonitor monitor, Object element, IDocument document, boolean overwrite)
 			throws CoreException {
-		System.out.println("doSaveDocument: " + element.toString() + ", document: " + document.toString());
-		String oldContent = document.get();
+		System.out.println("doSaveDocument: " + element.toString() + ", document: " + document.get());
 
-		// TODO flashing when saving could probably be prevented by cloning document and adapting the clone instead of changing the original document, saving and changing it back.
-		addDataToDoc(document);
-		
-		super.doSaveDocument(monitor, element, document, overwrite);
-		
-		document.set(oldContent);
+		try {
+			String saveData = TextModelIntegration.buildAnnotationXml(annotationProfile, annotationSet, document.get(), true);
+			super.doSaveDocument(monitor, element, new Document(saveData), overwrite);
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
 	}
 	
 
@@ -52,36 +56,20 @@ public class AnnotationDocumentProvider extends FileDocumentProvider {
 		
 		if (result) {
 			tmi = new TextModelIntegration(document);
-			annotationSet = new AnnotationSet(tmi.parseAnnotationData());
-			annotationProfile = tmi.parseAnnotationProfile();
-			removeDataFromDocument(document);
-			
+
+			try {
+				annotationSet = new AnnotationSet(TextModelIntegration.parseAnnotationData(document.get()));
+				annotationProfile = TextModelIntegration.parseAnnotationProfile(document.get());
+				document.set(TextModelIntegration.parseContent(document.get()));
+			} catch (ParserConfigurationException | IOException | SAXException e) {
+				e.printStackTrace();
+			}
+
 			initializeEvent.fire(new InitializeEvent(new AnnotationData(annotationSet, annotationProfile), document));
 			
 			return true;
 		} else {
 			return false;
 		}
-	}
-	
-	private void removeDataFromDocument(IDocument doc) {
-		String[] lines = doc.get().split("\n");
-		String[] outLines = new String[lines.length - 2];
-		for (int i = 0; i < lines.length - 2; i++) {
-			outLines[i] = lines[i + 2];
-		}
-		doc.set(Arrays.asList(outLines).stream().collect(Collectors.joining("\n")));
-	}
-	
-	private void addDataToDoc(IDocument doc) {
-		doc.set(tmi.buildAnnotationPrefix(annotationSet, annotationProfile) + doc.get());
-	}
-
-	public List<SingleAnnotation> getAnnotationData() {
-		return annotationSet.getAnnotations();
-	}
-
-	public AnnotationProfile getAnnotationProfile() {
-		return annotationProfile;
 	}
 }
