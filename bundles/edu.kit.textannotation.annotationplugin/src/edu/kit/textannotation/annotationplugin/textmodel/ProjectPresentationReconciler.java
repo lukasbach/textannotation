d13a6658bfc7dacdc5cb2163458f628adb99f4d2
Lukas Bach <lbach@outlook.de>
2020-01-14 01:45:22 +0100

    fix(text-model): fixed syntax highlighting bug

--- bundles/edu.kit.textannotation.annotationplugin/src/edu/kit/textannotation/annotationplugin/textmodel/ProjectPresentationReconciler.java
+++ bundles/edu.kit.textannotation.annotationplugin/src/edu/kit/textannotation/annotationplugin/textmodel/ProjectPresentationReconciler.java
@@ -1,5 +1,8 @@
 package edu.kit.textannotation.annotationplugin.textmodel;
 
+import edu.kit.textannotation.annotationplugin.EventManager;
+import org.eclipse.jface.text.*;
+import org.eclipse.jface.text.presentation.*;
 import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
@@ -12,99 +15,72 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.eclipse.jface.text.DocumentEvent;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IRegion;
-import org.eclipse.jface.text.ITypedRegion;
-import org.eclipse.jface.text.TextAttribute;
-import org.eclipse.jface.text.TextPresentation;
-import org.eclipse.jface.text.presentation.IPresentationDamager;
-import org.eclipse.jface.text.presentation.IPresentationRepairer;
-import org.eclipse.jface.text.presentation.PresentationReconciler;
 import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
 import org.eclipse.jface.text.rules.IRule;
 import org.eclipse.jface.text.rules.RuleBasedScanner;
 import org.eclipse.jface.text.rules.SingleLineRule;
 import org.eclipse.jface.text.rules.MultiLineRule;
 import org.eclipse.jface.text.rules.Token;
-import org.eclipse.jface.text.TextViewer;
 
-public class ProjectPresentationReconciler extends PresentationReconciler {
+public class ProjectPresentationReconciler implements IPresentationReconciler, IPresentationReconcilerExtension {
 	
 	private AnnotationProfile profile;
 	private AnnotationSet annotations;
-    
-    private class CustomDamagerRepairer implements IPresentationDamager, IPresentationRepairer  {
-    	private IDocument document;
-    	private IPresentationDamager damager = this;
-    	private IPresentationRepairer repairer = this;
-    	private TextModelIntegration parser;
-    	
-		@Override
-		public void setDocument(IDocument document) {
-			System.out.println("Reset document");
-			this.document = document;
-			this.parser = new TextModelIntegration(document);
-		}
 
-		@Override
-		public IRegion getDamageRegion(ITypedRegion partition, DocumentEvent event,
-				boolean documentPartitioningChanged) {
-			/*
-			 * event.getOffset: total offset where change happened
-			 * event.getLength: 
-			 */
-			
-			// System.out.println("offset" + event.getOffset() + " length" + event.getLength() + " text" + event.getText() + " plen" + partition.getLength() + " poff" + partition.getOffset());
-			// System.out.println("Marking as damaged: " + );
-			// return partition;
-			// TODO damaged region detected is currently very buggy...
-			System.out.println(String.format("Damage Region set to %s:%s", 0, document.getLength()));
-			return new IRegion() {
-				@Override public int getOffset() {
-					return 0;
-				}
-				@Override public int getLength() {
-					return document.getLength();
-				}
-			};
- 		}
+	private ITextListener textListener;
+	private ITextViewer textViewer;
 
+	private class TextListener implements ITextListener {
 		@Override
-		public void createPresentation(TextPresentation presentation, ITypedRegion damage) {
-			System.out.println(String.format("Creating presentation with the following annotations on damage %s:%s", damage.getOffset(), damage.getLength()));
+		public void textChanged(TextEvent event) {
 			List<SingleAnnotation> annotationList = annotations
 					.getAnnotations()
 					.stream()
 					.sorted(Comparator.comparingInt(SingleAnnotation::getOffset))
 					.collect(Collectors.toList());
-			
+
 			for (SingleAnnotation an: annotationList) {
 				try {
 					AnnotationClass ac = profile.getAnnotationClass(an.getAnnotationIdentifier());
-					System.out.println("Annotation: " + ac.toString() + ", offset=" + an.getOffset() + ", len=" + an.getLength());
-					presentation.addStyleRange(new StyleRange(
-							an.getOffset(), 
-							an.getLength(), 
-							ac.getColor(), 
-							new Color(Display.getCurrent(), new RGB(255, 255, 255))
-							));
+
+					textViewer.setTextColor(ac.getColor(), an.getOffset(), an.getLength(), true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-			System.out.print("\n\n");
 		}
-    	
-    }
+	}
+
+	public ProjectPresentationReconciler() {}
+
+	@Override
+	public void install(ITextViewer viewer) {
+		textListener = new TextListener();
+		textViewer = viewer;
+		textViewer.addTextListener(textListener);
+	}
+
+	@Override
+	public void uninstall() {
+		textViewer.removeTextListener(textListener);
+	}
+
+	@Override
+	public IPresentationDamager getDamager(String contentType) {
+		return null;
+	}
+
+	@Override
+	public IPresentationRepairer getRepairer(String contentType) {
+		return null;
+	}
+
+	@Override
+	public String getDocumentPartitioning() {
+		return null;
+	}
     
-    public ProjectPresentationReconciler() {
-        CustomDamagerRepairer dr = new CustomDamagerRepairer();
-        this.setDamager(dr, IDocument.DEFAULT_CONTENT_TYPE);
-        this.setRepairer(dr, IDocument.DEFAULT_CONTENT_TYPE);
-    }
-    
-    public void setAnnotationInformation(AnnotationProfile profile, AnnotationSet annotations) {
+    public void setAnnotationInformation(AnnotationProfile profile, AnnotationSet annotations) { // TODO use TextModelData
     	this.profile = profile;
     	this.annotations = annotations;
     }
