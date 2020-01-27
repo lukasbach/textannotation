fc80f2e2c23e52a106b5b73eb247d61107613997
Lukas Bach <lbach@outlook.de>
2020-01-27 23:38:17 +0100

    feat(text-model): profile datastructure supports metadata and descriptions

--- bundles/edu.kit.textannotation.annotationplugin/src/edu/kit/textannotation/annotationplugin/textmodel/TextModelIntegration.java
+++ bundles/edu.kit.textannotation.annotationplugin/src/edu/kit/textannotation/annotationplugin/textmodel/TextModelIntegration.java
@@ -8,8 +8,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import org.eclipse.jface.text.IDocument;
-
 import edu.kit.textannotation.annotationplugin.profile.AnnotationClass;
 import edu.kit.textannotation.annotationplugin.profile.AnnotationProfile;
 import org.w3c.dom.Document;
@@ -29,58 +27,13 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import static edu.kit.textannotation.annotationplugin.textmodel.XmlSchemaVariables.*;
+
 // TODO refactor visibilities, static methods, add internal state etc
 @SuppressWarnings("FieldCanBeLocal")
 public class TextModelIntegration {
 	private static SchemaValidator validator = new SchemaValidator();
 
-	/** The element name of the root element of an annotated file. */
-	private static final String KEY_ANNOTATEDFILE_ELEMENT = "annotated";
-
-	/** The element name of the content element in a annotated file. Child of an annotated tag. */
-	private static final String KEY_ANNOTATIONDATA_CONTENT = "content";
-
-	/** The element name of the element that references the used profile in an annotated file. Child of an annotated tag. */
-	private static final String KEY_ANNOTATEDFILE_PROFILE_ELEMENT = "annotationprofile";
-
-	/** The attribute key of the name of a profile in the annotationprofile tag which references the used profile in an annotated file. */
-	private static final String KEY_ANNOTATEDFILE_PROFILE_ATTR_NAME = "name";
-
-	/** The element name of the element that describes a specific annotation. Child of an annotated tag. */
-	private static final String KEY_ANNOTATIONDATA_ANNOTATION_ELEMENT = "annotation";
-
-	/** The attribute key of an annotations ID in a annotation element. */
-	private static final String KEY_ANNOTATIONDATA_ANNOTATION_ATTR_ID = "id";
-
-	/** The attribute key of an annotations offset in a annotation element. */
-	private static final String KEY_ANNOTATIONDATA_ANNOTATION_ATTR_OFFSET = "offset";
-
-	/** The attribute key of an annotations length in a annotation element. */
-	private static final String KEY_ANNOTATIONDATA_ANNOTATION_ATTR_LENGTH = "length";
-
-	/** The attribute key of an annotation class in a annotation element. */
-	private static final String KEY_ANNOTATIONDATA_ANNOTATION_ATTR_ANNOTATION_IDENTIFIER = "annotation";
-
-	/** The element name of the element that describes a key-value pair of metadata for one annotation. Child of an annotation element. */
-	private static final String KEY_ANNOTATIONDATA_METADATA_ELEMENT = "metadata";
-
-	/** The attribute key of the metadata key in a metadata element. */
-	private static final String KEY_ANNOTATIONDATA_METADATA_ATTR_NAME = "name";
-
-	/** The element name of the root element in a profile file. */
-	private static final String KEY_PROFILE_ELEMENT = "annotationprofile";
-
-	/** The attribute key of a profiles name on the root element in a profile file. */
-	private static final String KEY_PROFILE_ATTR_NAME = "name";
-
-	/** The element name of an annotation class in a profile file. Child of an annotationprofile element. */
-	private static final String KEY_PROFILE_ANNOTATIONCLASS_ELEMENT = "annotationclass";
-
-	/** The attribute key of an annotation classes name in an annotationclass element. */
-	private static final String KEY_PROFILE_ANNOTATIONCLASS_ATTR_NAME = "name";
-
-	/** The attribute key of an annotation classes color in an annotationclass element. */
-	private static final String KEY_PROFILE_ANNOTATIONCLASS_ATTR_COLOR = "color";
 
 	public static String parseContent(String rawSource) throws SchemaValidator.InvalidFileFormatException {
 		validator.validateAnnotatedFile(rawSource);
---
+++ bundles/edu.kit.textannotation.annotationplugin/src/edu/kit/textannotation/annotationplugin/textmodel/XmlSchemaVariables.java
@@ -0,0 +1,62 @@
+package edu.kit.textannotation.annotationplugin.textmodel;
+
+public class XmlSchemaVariables {
+    /** The element name of the root element of an annotated file. */
+    public static final String KEY_ANNOTATEDFILE_ELEMENT = "annotated";
+
+    /** The element name of the content element in a annotated file. Child of an annotated tag. */
+    public static final String KEY_ANNOTATIONDATA_CONTENT = "content";
+
+    /** The element name of the element that references the used profile in an annotated file. Child of an annotated tag. */
+    public static final String KEY_ANNOTATEDFILE_PROFILE_ELEMENT = "annotationprofile";
+
+    /** The attribute key of the name of a profile in the annotationprofile tag which references the used profile in an annotated file. */
+    public static final String KEY_ANNOTATEDFILE_PROFILE_ATTR_NAME = "name";
+
+    /** The element name of the element that describes a specific annotation. Child of an annotated tag. */
+    public static final String KEY_ANNOTATIONDATA_ANNOTATION_ELEMENT = "annotation";
+
+    /** The attribute key of an annotations ID in a annotation element. */
+    public static final String KEY_ANNOTATIONDATA_ANNOTATION_ATTR_ID = "id";
+
+    /** The attribute key of an annotations offset in a annotation element. */
+    public static final String KEY_ANNOTATIONDATA_ANNOTATION_ATTR_OFFSET = "offset";
+
+    /** The attribute key of an annotations length in a annotation element. */
+    public static final String KEY_ANNOTATIONDATA_ANNOTATION_ATTR_LENGTH = "length";
+
+    /** The attribute key of an annotation class in a annotation element. */
+    public static final String KEY_ANNOTATIONDATA_ANNOTATION_ATTR_ANNOTATION_IDENTIFIER = "annotation";
+
+    /** The element name of the element that describes a key-value pair of metadata for one annotation.
+     * Child of an annotation element. */
+    public static final String KEY_ANNOTATIONDATA_METADATA_ELEMENT = "metadata";
+
+    /** The attribute key of the metadata key in a metadata element of an annotated text file. */
+    public static final String KEY_ANNOTATIONDATA_METADATA_ATTR_NAME = "name";
+
+    /** The element name of the root element in a profile file. */
+    public static final String KEY_PROFILE_ELEMENT = "annotationprofile";
+
+    /** The attribute key of a profiles name on the root element in a profile file. */
+    public static final String KEY_PROFILE_ATTR_NAME = "name";
+
+    /** The element name of an annotation class in a profile file. Child of an annotationprofile element. */
+    public static final String KEY_PROFILE_ANNOTATIONCLASS_ELEMENT = "annotationclass";
+
+    /** The attribute key of an annotation classes name in an annotationclass element. */
+    public static final String KEY_PROFILE_ANNOTATIONCLASS_ATTR_NAME = "name";
+
+    /** The attribute key of an annotation classes color in an annotationclass element. */
+    public static final String KEY_PROFILE_ANNOTATIONCLASS_ATTR_COLOR = "color";
+
+    /** The element name of the description element inside of an annotation class element. */
+    public static final String KEY_PROFILE_ANNOTATIONCLASS_DESCRIPTION_ELEMENT = "description";
+
+    /** The element name of the element that describes a key-value pair of metadata for one annotation class.
+     * Child of an annotation class element. */
+    public static final String KEY_PROFILE_ANNOTATIONCLASS_METADATA_ELEMENT = "metadata";
+
+    /** The attribute key of the metadata key in a metadata element of a profile. */
+    public static final String KEY_PROFILE_ANNOTATIONCLASS_METADATA_ATTR_NAME = "name";
+}
